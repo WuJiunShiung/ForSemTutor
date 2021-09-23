c532329ebcb4fdaf837dbf9d1f54c495dcd94223
WuJiunShiung <wu.jiunshiung@gmail.com>
2021-09-23 21:51:59 +0800

    Ver 0.4
    把要印出的字典及字串加入

--- discord_bot_ForSemTutor02.py
+++ discord_bot_ForSemTutor02.py
@@ -21,6 +21,31 @@ with open("account.info", encoding="utf-8") as f:
     
 DISCORD_TOKEN=accountDICT["discord_token"]
 
+termDICT = {'lambda':'帶領一個可以用詞代入的變數，用法：lambda x [P(x)',
+            '全稱量詞': '帶領一個變數，是全部的意思，用法：forAll x [P(x)->Q(x[)]',
+            '存在量詞':'帶領一個變數，是有一個的意思，用法：thereIs x [P(x) and Q(x)]',
+            '專有名詞':"指涉叫那個名詞個體，用那個專有名詞來代表其語意",
+            '普通名詞':"具該名詞性質的個體所成之群組，用：lambda x [名詞(x)]來表示",
+            '及物動詞':"需要一個承受動作實體及一個執行動作實體的動作，寫成：lambda x lambda y [P(y, x)]",
+            '不及物動詞':"只需要一個執行動作的實體的動作，寫成：lambda x [P(x)]",
+            '謂語形容詞':'需要一個被談論的實體當主語的形容詞，寫成：lambda x [Adj(x)]',
+            '屬性形容詞':'修飾普通名詞的形容詞，寫成：lambda N lambda y [N(y) and Adj(y)]'}
+
+vpComp = """及物動詞：用代表承受動作的實體取代第一個 lambda 引領的變數，
+假設這個實體用"實體2"來表示，那就是：把 lambda x lambda y [P(y, x)]
+變成 lambda y [P(y, 實體2]；
+不及物動詞：直接變成動詞短語，這種動詞短語的語意等於不及物動詞語意。
+"""
+
+npComp = """專有名詞直接變成名詞短語，兩者語意相等，也就是專有名詞形成的
+名詞短語，語意就是專有名詞指涉的實體
+"""         
+
+sComp = """句子的語意由主語及動詞短語組合而成；由代表主語的實體取代動詞短語
+中的 lambda 引領的變數，如主詞寫為：實體1，動詞短語的語意 lambda y [P(y, 實體2)]
+變為：[P(實體1, 實體2)]
+"""         
+
 # 取得多輪對話資訊
 
 client = discord.Client()
@@ -80,12 +105,11 @@ async def on_message(message):
             
         # 處理時間差
         datetimeNow = datetime.datetime.now()  # 取得當下時間
-        timeDIFF = datetimeNow - mscDICT[client.user.id]["updatetime"]
+        timeDIFF = datetimeNow - mscDICT[client.user.id]["time"]
         if timeDIFF.total_seconds() <= 300:    # 以秒為單位，5分鐘以內都算是舊對話
-            mscDICT[client.user.id]["updatetime"] = datetimeNow 
+            mscDICT[client.user.id]["time"] = datetimeNow 
             
-        # 清空templateDICT還沒做，得想想清空的時機
-            
+                  
         
         #多輪對話
         if lokiResultDICT:
@@ -127,7 +151,7 @@ async def on_message(message):
                     
         elif mscDICT[client.user.id]['action'] == "comp":
             if mscDICT[client.user.id]['term'] == "句子":
-                await messeage.reply(sComp)
+                await messege.reply(sComp)
             elif mscDICT[client.user.id]['term'] == "動詞":
                 await message.reply(vpComp)
             elif mscDICT[client.user.id]['term'] == "名詞":
@@ -140,7 +164,7 @@ async def on_message(message):
             
         del mscDICT[client.user.id]
         
-if __name__ == "__maiin__":
+if __name__ == "__main__":
     client.run(DISCORD_TOKEN)
                     
                     
